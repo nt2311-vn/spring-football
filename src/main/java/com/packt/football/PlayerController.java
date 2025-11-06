6d547dde7e265535ab01619289a93da97ed3b722
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-06 17:43:31 +0700

    add postmapping and getmapping with map wildcard

--- src/main/java/com/packt/football/PlayerController.java
+++ src/main/java/com/packt/football/PlayerController.java
@@ -2,6 +2,9 @@ package com.packt.football;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,15 @@ public class PlayerController {
   @GetMapping
   public List<String> listPlayers() {
     return List.of("Ivana ANDES", "Alexia PUTELLAS");
+  }
+
+  @PostMapping
+  public String createPlayer(@RequestBody String name) {
+    return System.out.format("Player %s created", name).toString();
+  }
+
+  @GetMapping("/{name}")
+  public String readPlayer(@PathVariable String name) {
+    return name;
   }
 }
