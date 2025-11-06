aadeb7289fe6a73492fdee9bb866598d23de892e
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-06 17:49:06 +0700

    add mapping for delete and putmapping

--- src/main/java/com/packt/football/PlayerController.java
+++ src/main/java/com/packt/football/PlayerController.java
@@ -1,9 +1,11 @@
 package com.packt.football;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,15 @@ public class PlayerController {
   @GetMapping("/{name}")
   public String readPlayer(@PathVariable String name) {
     return name;
+  }
+
+  @DeleteMapping("/{name}")
+  public String deletePlayer(@PathVariable String name) {
+    return System.out.format("Player %s deleted", name).toString();
+  }
+
+  @PutMapping("/{name}")
+  public String updatePlayer(@PathVariable String name, @RequestBody String newName) {
+    return System.out.format("Player %s updated to %s", name, newName).toString();
   }
 }
