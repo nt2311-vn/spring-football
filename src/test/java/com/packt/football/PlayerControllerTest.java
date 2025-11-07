225d9773cc7ba16d8d773fe2ef720dd8c3f2b480
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-07 15:56:18 +0700

    replace the mockbean as deprecated

--- src/test/java/com/packt/football/PlayerControllerTest.java
+++ src/test/java/com/packt/football/PlayerControllerTest.java
@@ -15,8 +15,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,7 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @WebMvcTest(value = PlayerController.class)
 public class PlayerControllerTest {
   @Autowired private MockMvc mvc;
-  @MockBean private FootballService footballService;
+  @MockitoBean private FootballService footballService;
 
   @Test
   public void testListPlayers() throws Exception {
