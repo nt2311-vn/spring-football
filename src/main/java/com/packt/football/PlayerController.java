5c020ae5096bbb66a3097010dac328d898322a96
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-07 11:50:59 +0700

    update player controller using response with response entity

--- src/main/java/com/packt/football/PlayerController.java
+++ src/main/java/com/packt/football/PlayerController.java
@@ -1,15 +1,21 @@
 package com.packt.football;
 
+import com.packt.football.exceptions.AlreadyExistsException;
+import com.packt.football.exceptions.NotFoundException;
 import com.packt.football.model.Player;
 import com.packt.football.services.FootballService;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/players")
@@ -27,13 +33,15 @@ public class PlayerController {
   }
 
   @GetMapping("/{id}")
-  public Player readPlayer(@PathVariable String id) {
-    return footballService.getPlayer(id);
+  public ResponseEntity<Player> readPlayer(@PathVariable String id) {
+    Player player = footballService.getPlayer(id);
+    return new ResponseEntity<>(player, HttpStatus.OK);
   }
 
   @PostMapping
-  public void createPlayer(@RequestBody Player player) {
-    footballService.addPlayer(player);
+  public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
+    Player addedPlayer = footballService.addPlayer(player);
+    return new ResponseEntity<>(addedPlayer, HttpStatus.ACCEPTED);
   }
 
   @DeleteMapping("/{id}")
@@ -45,4 +53,12 @@ public class PlayerController {
   public void updatePlayer(@PathVariable String id, @RequestBody Player player) {
     footballService.updatePlayer(player);
   }
+
+  @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found")
+  @ExceptionHandler(NotFoundException.class)
+  public void notFoundHandler() {}
+
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Already exists")
+  @ExceptionHandler(AlreadyExistsException.class)
+  public void alreadyExistsHandler() {}
 }
