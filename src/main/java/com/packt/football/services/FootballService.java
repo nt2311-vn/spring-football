523afe49962a6f3143671586d196558348187b33
nt2311-vn <nguyentoan231196@gmail.com>
2025-11-07 10:51:36 +0700

    create football service to return map or modify map data in memory of players

---
+++ src/main/java/com/packt/football/services/FootballService.java
@@ -0,0 +1,51 @@
+package com.packt.football.services;
+
+import com.packt.football.exceptions.AlreadyExistsException;
+import com.packt.football.exceptions.NotFoundException;
+import com.packt.football.model.Player;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FootballService {
+  private final Map<String, Player> players =
+      Map.ofEntries(
+          Map.entry(
+              "1884823",
+              new Player("1884823", 5, "Ivana ANDRES", "Defender", LocalDate.of(1994, 07, 13))),
+          Map.entry(
+              "325636",
+              new Player(
+                  "325636", 11, "Alexia PUTELAS", "Midfielder", LocalDate.of(1994, 02, 04))));
+
+  public List<Player> listPlayers() {
+    return players.values().stream().collect(Collectors.toList());
+  }
+
+  public Player getPlayer(String id) {
+    Player player = players.get(id);
+    if (player == null) {
+      throw new NotFoundException("Player not found");
+    }
+
+    return player;
+  }
+
+  public Player addPlayer(Player player) {
+    if (players.containsKey(player.id())) {
+      throw new AlreadyExistsException("The player already exists");
+    } else {
+      players.put(player.id(), player);
+      return player;
+    }
+  }
+
+  public void deletePlayer(String id) {
+    if (players.containsKey(id)) {
+      players.remove(id);
+    }
+  }
+}
